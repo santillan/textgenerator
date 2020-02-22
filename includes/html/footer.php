7ebf156a841e8e190c5f3b1461a9ee3dfdcad86e
santillan <santillan@markium.com>
2020-02-21 21:47:11 -0500

    Add files via upload

---
+++ includes/html/footer.php
@@ -0,0 +1,43 @@
+<!-- website footer -->
+<div id="footer">
+
+<?php
+/*
+   <div>
+      <div class="row container">
+         <div class="col4">
+            <span>social media</span>
+            <div>
+               <a href="<?php echo $website['facebook-url'];?>" target="_blank">facebook</a>
+               <a href="<?php echo $website['twitter-url'];?>" target="_blank">twitter</a>
+               <a href="<?php echo $website['pinterest-url'];?>" target="_blank">pinterest</a>
+               <a href="<?php echo $website['google-url'];?>" target="_blank">google+</a>
+            </div>
+         </div>
+         <div class="col4">
+            <span><?php echo $website['name'];?></span>
+            <div>
+               <a href="<?php echo $website['url'];?>">home</a>
+               <a href="mailto:<?php echo $website['email'];?>">contact</a>
+            </div>
+         </div>
+         <div class="col4">
+            <span>about us</span>
+            <div>
+               <p><?php echo $website['about'];?></p>
+            </div>
+         </div>
+      </div>
+   </div>
+ 
+
+   <div>
+      <div class="row container">
+         <div class="col12">
+            <p>copyright <?php echo $website['year'];?>, <?php echo $website['name'];?> all rights reserved.</p>
+         </div>
+      </div>
+   </div>
+  */
+  ?>
+</div>
---
+++ includes/html/header.php
@@ -0,0 +1,16 @@
+<!-- website header -->
+<div id="header">
+   <div class="row container">
+
+<?php
+/*
+      <div class="col4">
+         <a href="<?php echo $website['url'];?>">
+            <img src="<?php echo $website['url'];?>assets/img/logo.png?v=<?php echo $website['version'];?>" alt="<?php echo $website['name'];?>" />
+         </a>
+      </div>
+*/
+?>	  
+
+   </div>
+</div>
